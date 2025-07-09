1ec5ad838203e9f217dde1884ad0ebb89d249f74
Erick-Nascimento <55209050+Erick-Nascimento@users.noreply.github.com>
2025-07-09 11:41:39 -0300

    feat: add tiktok meta and linkedin pixels

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -141,6 +141,7 @@ export default defineApp(async (_req, ctx) => {
 
         {/* Web Manifest */}
         <link rel="manifest" href={asset("/site.webmanifest")} />
+
         {/* Google Tag Manager */}
         <script
           async
@@ -151,7 +152,101 @@ export default defineApp(async (_req, ctx) => {
           {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date()); gtag('config', 'GTM-PXCP9QB');`}
         </script>
+        {/* LinkedIn Pixel */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+      _linkedin_partner_id = "1241034";
+      window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+      window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+    `,
+          }}
+        />
 
+        <script
+          type="text/javascript"
+          src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
+          async
+        >
+        </script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              `<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1241034&fmt=gif" />`,
+          }}
+        />
+
+        {/* TikTok Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function (w, d, t) {
+        w.TiktokAnalyticsObject = t;
+        var ttq = w[t] = w[t] || [];
+        ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"];
+        ttq.setAndDefer = function(t, e) {
+          t[e] = function() {
+            t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+          };
+        };
+        for (var i = 0; i < ttq.methods.length; i++) {
+          ttq.setAndDefer(ttq, ttq.methods[i]);
+        }
+        ttq.instance = function(t) {
+          var e = ttq._i[t] || [];
+          for (var n = 0; n < ttq.methods.length; n++) {
+            ttq.setAndDefer(e, ttq.methods[n]);
+          }
+          return e;
+        };
+        ttq._i = {};
+        ttq.load = function(e, n) {
+          var i = "https://analytics.tiktok.com/i18n/pixel/events.js";
+          ttq._i[e] = [];
+          ttq._i[e]._u = i;
+          ttq._t = ttq._t || {};
+          ttq._t[e] = +new Date;
+          ttq._o = ttq._o || {};
+          ttq._o[e] = n || {};
+          var a = d.createElement("script");
+          a.type = "text/javascript";
+          a.async = true;
+          a.src = i + "?sdkid=" + e + "&lib=" + t;
+          var s = d.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(a, s);
+        };
+        ttq.load("CATN8D3C77U5NQUHEE30");
+        ttq.page();
+      }(window, document, "ttq");
+    `,
+          }}
+        />
+
+        {/* Meta (Facebook) Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1458020004516977');
+      fbq('track', 'PageView');
+    `,
+          }}
+        />
+
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1458020004516977&ev=PageView&noscript=1" />`,
+          }}
+        />
         {/* CSS do Leaflet */}
         <link
           rel="stylesheet"
